8b95fa18c818ac8986e335561d59cf50f5c03aa0
ChalkPE <chalk@chalk.pe>
2017-07-05 22:56:11 +0900

    Read data from file

--- app/App.vue
+++ app/App.vue
@@ -1,16 +1,29 @@
 <template lang="pug">
   #app
-    h1 Hello, world!
-    button.button(type='button') Wow
+    main: .container
+      table.table
+        tr(v-for='item in list')
+          td {{ item }}
 </template>
 
 <script>
+  import fs from 'fs'
+  import path from 'path'
   import electron from 'electron'
-  const { shell } = electron.remote
+  import promisify from 'util.promisify'
+
+  const { app } = electron.remote
+  const readFile = promisify(fs.readFile)
 
   export default {
-    created () {
-      shell.openExternal('https://github.com/ChalkPE')
+    data: () => ({ list: [] }),
+
+    async created () {
+      const home = app.getPath('home')
+      const file = path.join(home, 'favorites.txt')
+      const data = await readFile(file, 'utf-8')
+
+      this.list = data.split('\n').filter(x => x)
     }
   }
 </script>
